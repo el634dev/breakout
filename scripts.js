b22fca6e9d62da68716b4f95e80cbd87664c5ffa
el634dev <84924260+el634dev@users.noreply.github.com>
2024-01-16 13:40:06 -0500

    Completed Part 2 of the MDN Breakout Tutorial

--- scripts.js
+++ scripts.js
@@ -3,29 +3,36 @@ const canvas = document.getElementById("gameCanvas");
 // Stores the rendering context, tool we use to paint
 const ctx = canvas.getContext("2d");
 
-// Rectangle Drawing
-ctx.beginPath();
-// Draw a rectangle/perfect square
-ctx.rect(20, 40, 50, 50);
-// Store a color
-ctx.fillStyle = "#FF0000";
-// Uses the above color to paint the sqaure
-ctx.fill();
-ctx.closePath();
+// Set x to the width of the Canvas / 2
+let x = canvas.width / 2;
+// Set y to the height of the Canvas - 30
+let y = canvas.height - 30;
 
-// Green Circle Drawing
-ctx.beginPath();
-// Draw a rectangle/perfect square
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-// Store a color
-ctx.fillStyle = "green";
-// Uses the above color to paint the sqaure
-ctx.fill();
-ctx.closePath();
+// Small values to create motion
+let dx = 2;
+let dy = -2;
 
-// Blue Rectangle
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+// Create shape 
+function drawBall() {
+    // Draw shape and fill
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    // Close drawing
+    ctx.closePath();
+}
+
+function draw() {
+    // Clear area before drawing new shape
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // Create shape
+    drawBall();
+
+    // Update the x and y
+    x += dx;
+    y += dy;
+}
+
+// Executed within setInterval every 10 milliseconds
+setInterval(draw, 10);
